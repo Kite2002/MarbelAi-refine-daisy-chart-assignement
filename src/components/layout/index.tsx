e9902097b32514d6918a4866326baa38e025b002
Debabrata <dbatabyal218@gmail.com>
2024-03-02 10:30:24 +0530

    readded menu and other components ,  edited menu font size

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ import { Menu } from "../menu";
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="">
-      {/* <Menu /> */}
+      <Menu />
       <div className="p-4 bg-zinc-100">
         <Breadcrumb />
         <div>{children}</div>
