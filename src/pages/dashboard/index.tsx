95b6fcc4adaa8340e08d842a79a0d58c47ca8af0
Debabrata <dbatabyal218@gmail.com>
2024-03-03 20:21:32 +0530

    dropddown interaction improvements

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -121,12 +121,12 @@ export const Dashboard: React.FC = () => {
 
       <div className="bg-white  shadow-[0px_1px_2px_0px_#00000040] py-7 space-y-9  rounded-[10px] px-4 pr-8 md:px-12 md:py-4">
         <div className="flex justify-between items-center">
-          <div className="w-[95%]  grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-[25px] ">
+          <div className="w-[95%] grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-[25px] ">
             {[1, 2, 3, 4].map(() => {
               return (
-                <div className="px-4 flex-1 group  transition-all duration-300 py-2 hover:bg-[#F1F1F1] bg-white rounded-[10px] gap-[25px]">
+                <div className="px-4 first-of-type:bg-[#f1f1f1] flex-1 group  transition-all duration-300 py-2 focus-within:bg-[#F1F1F1] hover:bg-[#F1F1F1] bg-white rounded-[10px] flex-col space-y-[5px] ">
                   <div className="flex justify-between items-center">
-                    <span className="text-[14px] font-medium text-[#303030]">
+                    <span className=" border-b-[1px] border-[#CCCCCC] border-dashed text-[14px] font-medium text-[#303030]">
                       Online store sessions
                     </span>
 
@@ -134,20 +134,24 @@ export const Dashboard: React.FC = () => {
                       className="dropdown group-even:dropdown-end
                     "
                     >
-                      <button
-                        className="group-hover:opacity-100 opacity-0
-transition-opacity duration-300 rounded-md p-[5px] hover:bg-[#CBC8C8]"
-                      >
+                      <button className="group-hover:opacity-100 group-first:opacity-100 opacity-0  focus-within:opacity-100 focus-within:bg-[#CBC8C8] transition-opacity duration-300 rounded-md p-[5px] hover:bg-[#CBC8C8]">
                         <PencilIcon
                           color="#00000080"
                           className="   w-[15px] h-[15px] "
                         />
                       </button>
-                      <ul className="shadow menu dropdown-content gap-[5px] z-[1] bg-base-100 rounded-[10px] ">
+                      <ul className="shadow menu dropdown-content top-8 gap-[5px] z-[1] bg-base-100 rounded-[10px] ">
                         {[1, 2, 3, 4].map((item, index) => {
                           return (
                             <li className="flex justify-between">
-                              <button className="flex text-xs text-[#303030] justify-between">
+                              <button
+                                onClick={() =>
+                                  (
+                                    document?.activeElement as HTMLElement
+                                  )?.blur()
+                                }
+                                className="flex text-xs text-[#303030] justify-between"
+                              >
                                 <div className="flex items-center gap-x-[10px]">
                                   <PresentationChartLineIcon className="h-4 w-4" />
                                   <p className=" text-nowrap">
